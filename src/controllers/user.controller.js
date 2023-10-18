f6d2743c166baf1b678fadfb74684e78067871da
Your Name~ <you@example.com>
2023-10-18 11:10:34 -0300

    Modificacion en el archivo user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,18 +4,11 @@ import UserModel from "../models/user.model";
 // Define los controladores
 const createUser = async (req, res) => {
     try {
-        // Realiza las operaciones necesarias, como crear un usuario en la base de datos
-        // utilizando el modelo correspondiente (UserModel)
-        // Ejemplo:
         const user = await UserModel.create(req.body);
-
-        // Envía la respuesta correspondiente
         return res.status(200).send({ mensaje: 'Usuario creado' });
     } catch (error) {
-        // Maneja los errores y envía una respuesta de error en caso de que ocurra algún problema
         return res.status(500).send({ mensaje: `Error al crear usuario: ${error}` });
     }
 };
 
-// Exporta los controladores para que puedan ser utilizados por otros componentes
 export { createUser };
